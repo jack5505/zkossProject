ad233371d152768de120f8287ee96f64b732415b
Sabirov Jakhongir <jahon.080879@gmail.com>
2021-11-08 12:04:55 +0500

    Init project

---
+++ src/main/java/org/uz/ofb/service/TestService.java
@@ -0,0 +1,12 @@
+package org.uz.ofb.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TestService {
+	public Date getTime() {
+		return new Date();
+	}
+}
